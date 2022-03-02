43606ed41656dee90f1e39b7ad9f231c742ba10d
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-03-01 18:04:05 -0800

    really done

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,8 +1,8 @@
 const database = include('/databaseConnection');
 
 
-function getAllRestaurant(callback) {
-	let sqlQuery = "SELECT * FROM restaurant";
+function getAllRestaurants(callback) {
+	let sqlQuery = "SELECT restaurant_id, name, description FROM restaurant";
 	database.query(sqlQuery, (err, results, fields) => {
 		if (err) {
 			callback(err, null);
@@ -14,9 +14,13 @@ function getAllRestaurant(callback) {
 	});
 }
 
-function getAllreviews(callback) {
-	let sqlQuery = "SELECT * FROM review";
-	database.query(sqlQuery, (err, results, fields) => {
+function deleteRestaurant(restaurant_id, callback) {
+	let sqlDeleteRestaurant = "DELETE FROM restaurant WHERE restaurant_id = :userID";
+	let params = {
+		userID: restaurant_id
+	};
+	console.log(sqlDeleteRestaurant);
+	database.query(sqlDeleteRestaurant, params, (err, results, fields) => {
 		if (err) {
 			callback(err, null);
 		}
@@ -26,9 +30,9 @@ function getAllreviews(callback) {
 		}
 	});
 }
-const passwordPepper = "SeCretPeppa4MySal+";
-function addUser(postData, callback) {
-	let sqlInsertSalt = "INSERT INTO restaurant (name, description)VALUES (:name, :description);"
+
+function addRestaurant(postData, callback) { 
+	let sqlInsertSalt = "INSERT INTO restaurant (name, description) VALUES (:name, :description);"
 	let params = {
 		name: postData.name,
 		description: postData.description
@@ -38,37 +42,17 @@ function addUser(postData, callback) {
 		if (err) {
 			console.log(err);
 			callback(err, null);
-		}
-		else {
-			let insertedID = results.insertId;
-			let updatePasswordHash = "UPDATE restaurant SET password_hash = sha2(concat(:password,:pepper,password_salt),512) WHERE restaurant_id = :name;"
-			let params2 = {
-				password: postData.password,
-				pepper: passwordPepper,
-				userId: insertedID
-			}
-			console.log(updatePasswordHash);
-			database.query(updatePasswordHash, params2, (err, results, fields) => {
-				if (err) {
-					console.log(err);
-					callback(err, null);
-				}
-				else {
-					console.log(results);
-					callback(null, results);
-				}
-			});
+		} else {
+			console.log(results);
+			callback(null, results);
 		}
 	});
 }
 
-function deleteUser(restaurantUserId, callback) {
-	let sqlDeleteUser = "DELETE FROM restaurant WHERE restaurant_id = :userID";
-	let params = {
-		userID: restaurantUserId
-	};
-	console.log(sqlDeleteUser);
-	database.query(sqlDeleteUser, params, (err, results, fields) => {
+
+function getAllReviews(callback) {
+	let sqlQuery = "SELECT review_id, restaurant_id, reviewer_name, details, rating FROM review";
+	database.query(sqlQuery, (err, results, fields) => {
 		if (err) {
 			callback(err, null);
 		}
@@ -79,4 +63,43 @@ function deleteUser(restaurantUserId, callback) {
 	});
 }
 
-module.exports = { getAllRestaurant, getAllreviews, deleteUser, addUser }
+
+function deleteReview(review_id, callback) {
+	let sqlDeleteReview = "DELETE FROM review WHERE review_id = :review_id";
+	let params = {
+		review_id: review_id
+	};
+	console.log(sqlDeleteReview);
+	database.query(sqlDeleteReview, params, (err, results, fields) => {
+		if (err) {
+			callback(err, null);
+		}
+		else {
+			console.log(results);
+			callback(null, results);
+		}
+	});
+}
+
+function addReview(postData, callback) { 
+	let sqlInsertSalt = "INSERT INTO review (restaurant_id, reviewer_name, details, rating) VALUES (:restaurant_id, :reviewer_name, :details, :rating);"
+	let params = {
+		restaurant_id: postData.restaurant_id,
+		reviewer_name: postData.name,
+		details: postData.details,
+		rating: postData.rating,
+	};
+	console.log(sqlInsertSalt);
+	database.query(sqlInsertSalt, params, (err, results, fields) => {
+		if (err) {
+			console.log(err);
+			callback(err, null);
+		} else {
+			console.log(results);
+			callback(null, results);
+		}
+	});
+}
+
+
+module.exports = { getAllRestaurants, deleteRestaurant, addRestaurant, getAllReviews, deleteReview, addReview }
--- routes/router.js
+++ routes/router.js
@@ -12,14 +12,14 @@ router.get('/', (req, res) => {
             console.log(err);
         }
         else {
-            dbModel.getAllRestaurant((err, result) => {
+            dbModel.getAllRestaurants((err, result) => {
                 if (err) {
                     res.render('error', { message: 'Error reading from MySQL' });
                     console.log("Error reading from mysql");
                     console.log(err);
                 }
                 else { //success
-                    res.render('index', { allRestaurant: result });
+                    res.render('index', { allRestaurants: result });
 
                     //Output the results of the query to the Heroku Logs
                     console.log(result);
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/addUser', (req, res) => {
+router.post('/addRestaurant', (req, res) => {
     console.log("form submit");
     database.getConnection(function (err, dbConnection) {
         if (err) {
@@ -41,7 +41,7 @@ router.post('/addUser', (req, res) => {
         }
         else {
             console.log(req.body);
-            dbModel.addUser(req.body, (err, result) => {
+            dbModel.addRestaurant(req.body, (err, result) => {
                 if (err) {
                     res.render('error', { message: 'Error writing to MySQL' });
                     console.log("Error writing to mysql");
@@ -58,68 +58,8 @@ router.post('/addUser', (req, res) => {
     });
 });
 
-router.get('/showReviews', (req, res) => {
-    console.log("page hit");
-    database.getConnection(function (err, dbConnection) {
-        if (err) {
-            res.render('error', { message: 'Error connecting to MySQL' });
-            console.log("Error connecting to mysql");
-            console.log(err);
-        }
-        else {
-            dbModel.getAllreviews((err, result) => {
-                if (err) {
-                    res.render('error', { message: 'Error reading from MySQL' });
-                    console.log("Error reading from mysql");
-                    console.log(err);
-                }
-                else { //success
-                    res.render('show', { allReviews: result });
-
-                    //Output the results of the query to the Heroku Logs
-                    console.log(result);
-                }
-            });
-            dbConnection.release();
-        }
-    });
-});
-
-
-
-router.post('/addUser', (req, res) => {
-    console.log("form submit");
-    database.getConnection(function (err, dbConnection) {
-        if (err) {
-            res.render('error', { message: 'Error connecting to MySQL' });
-            console.log("Error connecting to mysql");
-            console.log(err);
-        }
-        else {
-            console.log(req.body);
-            dbModel.addUser(req.body, (err, result) => {
-                if (err) {
-                    res.render('error', { message: 'Error writing to MySQL' });
-                    console.log("Error writing to mysql");
-                    console.log(err);
-                }
-                else { //success
-                    res.redirect("/");
-                    //Output the results of the query to the Heroku Logs
-                    console.log(result);
-                }
-            });
-            dbConnection.release();
-        }
-    });
-});
-
-
-       
-
-
-router.get('/deleteUser', (req, res) => {
-    console.log("delete user");
+router.get('/deleteRestaurant', (req, res) => {
+    console.log("delete restaurant");
     database.getConnection(function (err, dbConnection) {
         if (err) {
             res.render('error', { message: 'Error connecting to MySQL' });
@@ -128,9 +68,9 @@ router.get('/deleteUser', (req, res) => {
         }
         else {
             console.log(req.query);
-            let userId = req.query.id;
-            if (userId) {
-                dbModel.deleteUser(userId, (err, result) => {
+            let restaurant_id = req.query.id;
+            if (restaurant_id) {
+                dbModel.deleteRestaurant(restaurant_id, (err, result) => {
                     if (err) {
                         res.render('error', { message: 'Error writing to MySQL' });
                         console.log("Error writing to mysql");
@@ -146,9 +86,104 @@ router.get('/deleteUser', (req, res) => {
             else {
                 res.render('error', { message: 'Error on Delete' });
             }
-
             dbConnection.release();
         }
     });
 });
+
+router.get('/showReviews', (req, res) => {
+    console.log("page hit");
+    database.getConnection(function (err, dbConnection) {
+        if (err) {
+            res.render('error', { message: 'Error connecting to MySQL' });
+            console.log("Error connecting to mysql");
+            console.log(err);
+        }
+        else {
+            dbModel.getAllReviews((err, result) => {
+                if (err) {
+                    res.render('error', { message: 'Error reading from MySQL' });
+                    console.log("Error reading from mysql");
+                    console.log(err);
+                }
+                else { //success
+                    let restaurant_id = req.query.id
+                    let restaurant_name = req.query.name;
+                    res.render('show', { restaurant_id, restaurant_name, allReviews: result });
+                    //Output the results of the query to the Heroku Logs
+                    console.log(result);
+
+                }
+            });
+            dbConnection.release();
+        }
+    });
+});
+
+router.post('/addReview', (req, res) => {
+    console.log("form submit");
+    database.getConnection(function (err, dbConnection) {
+        if (err) {
+            res.render('error', { message: 'Error connecting to MySQL' });
+            console.log("Error connecting to mysql");
+            console.log(err);
+        }
+        else {
+            console.log(req.body);
+            dbModel.addReview(req.body, (err, result) => {
+                if (err) {
+                    res.render('error', { message: 'Error writing to MySQL' });
+                    console.log("Error writing to mysql");
+                    console.log(err);
+                }
+                else { //success
+                    let restaurant_id = req.query.id
+                    let restaurant_name = req.query.name
+                    res.redirect(`/showReviews?id=${restaurant_id}&name=${restaurant_name}`);
+                    //Output the results of the query to the Heroku Logs
+                    console.log(result);
+                }
+            });
+            dbConnection.release();
+        }
+    });
+});
+
+router.get('/deleteReview', (req, res) => {
+    console.log("delete review");
+    database.getConnection(function (err, dbConnection) {
+        if (err) {
+            res.render('error', { message: 'Error connecting to MySQL' });
+            console.log("Error connecting to mysql");
+            console.log(err);
+        }
+        else {
+            console.log(req.query);
+            let restaurant_id  = req.query.id;
+            let restaurant_name= req.query.name;
+            let review_id = req.query.review_id;
+            if (review_id) {
+                dbModel.deleteReview(review_id, (err, result) => {
+                    if (err) {
+                        res.render('error', { message: 'Error writing to MySQL' });
+                        console.log("Error writing to mysql");
+                        console.log(err);
+                    }
+                    else { //success
+                        res.redirect(`/showReviews?id=${restaurant_id}&name=${restaurant_name}`);
+                        //Output the results of the query to the Heroku Logs
+                        console.log(result);
+                    }
+                });
+            }
+            else {
+                res.render('error', { message: 'Error on Delete' });
+            }
+            dbConnection.release();
+        }
+    });
+});
+       
+
+
 module.exports = router;
